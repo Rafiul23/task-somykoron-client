f1385831eb2f6a8e422f3d86dc7b0719bd20c3b6
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-04-05 16:31:04 +0600

    added message form

--- src/Components/WriteMessage/WriteMessage.jsx
+++ src/Components/WriteMessage/WriteMessage.jsx
@@ -1,9 +1,31 @@
+import Button from "../Button/Button";
+import SectionTitle from "../SectionTitile/SectionTitle";
 
 
 const WriteMessage = () => {
     return (
         <div>
-            
+            <SectionTitle
+                title='Write Your Message'
+            ></SectionTitle>
+
+            <div>
+                
+
+                <div className="bg-base-200 w-1/2 my-5 mx-auto p-4">
+                    <form>
+                        <h2 className="text-blue-900 font-bold text-3xl ">Your Name: </h2>
+                        <input type="text" placeholder="Your Name" name="name" className="border p-4 block my-4 w-full" required />
+                        <br />
+                        <h2 className="text-blue-900 font-bold text-3xl">Your Message: </h2>
+                        <textarea name="message" id="message" placeholder="Your message" className="border p-4 block my-4 w-full" cols="30" rows="10"></textarea>
+
+                       <Button
+                       title='Submit'
+                       ></Button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Router/Router.jsx';
+import MessageProvider from './Components/MessageProvider/MessageProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <MessageProvider>
     <RouterProvider router={router} />
+    </MessageProvider>
   </React.StrictMode>,
 )
