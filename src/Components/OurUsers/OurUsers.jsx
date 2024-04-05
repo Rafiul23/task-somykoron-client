63f44a380899f1db35b401fbbed7cb1487cf46eb
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-04-05 12:24:00 +0600

    added fake json data

---
+++ src/Components/OurUsers/OurUsers.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../SectionTitile/SectionTitle";
+
+
+const OurUsers = () => {
+    return (
+        <div>
+            <SectionTitle
+            title='Our Users'
+            ></SectionTitle>
+        </div>
+    );
+};
+
+export default OurUsers;
---
+++ src/Components/SectionTitile/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+
+const SectionTitle = ({title}) => {
+    return (
+        <div className="text-center my-5">
+            <h2 className="lg:text-5xl md:text-3xl text-2xl text-blue-900 font-bold">{title}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
