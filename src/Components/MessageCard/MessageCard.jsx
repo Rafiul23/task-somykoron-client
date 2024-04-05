52e2e1ca9e48972ed62e23df1c928484b5960719
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-04-05 19:40:48 +0600

    added delete functionality

--- src/Components/MessageCard/MessageCard.jsx
+++ src/Components/MessageCard/MessageCard.jsx
@@ -1,10 +1,12 @@
-import Button from "../Button/Button";
+import { useContext } from "react";
+import { MessageContext } from "../MessageProvider/MessageProvider";
 
 
 const MessageCard = ({messageData}) => {
 
-    const {name, message} = messageData;
-    console.log(messageData);
+    const {handleDeleteMessage} = useContext(MessageContext);
+
+    const {_id, name, message} = messageData;
 
     return (
         <div className="card w-full bg-base-200 shadow-lg">
@@ -12,9 +14,8 @@ const MessageCard = ({messageData}) => {
                 <h2 className="card-title text-blue-900">{name}</h2>
                 <p>{message}</p>
                 <div className="card-actions justify-end">
-                    <Button
-                        title='Delete Message'
-                    ></Button>
+                    <button className="btn bg-blue-900 text-white" onClick={()=> handleDeleteMessage(_id)}
+                    >Delete Message</button>
                 </div>
             </div>
         </div>
--- src/Components/MessageProvider/MessageProvider.jsx
+++ src/Components/MessageProvider/MessageProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-
+import Swal from 'sweetalert2'
 
 export const MessageContext = createContext(null);
 
@@ -13,10 +13,35 @@ const MessageProvider = ({children}) => {
             .then(data => setMessages(data))
     }, [])
 
+    const handleDeleteMessage = (_id) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+            const remaining = messages.filter(message => message._id !== _id);
+
+            setMessages([...remaining]);
+
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+    }
 
     const messageInfo = {
         messages,
-        setMessages
+        setMessages,
+        handleDeleteMessage
     }
 
     return (
