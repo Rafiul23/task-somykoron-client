a6b8be87ead1f7916240c9d00f4e5e5314356125
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-04-05 12:33:25 +0600

    fetched data from json

--- src/Components/OurUsers/OurUsers.jsx
+++ src/Components/OurUsers/OurUsers.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitile/SectionTitle";
+import UserCard from "../UserCard/UserCard";
 
 
 const OurUsers = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('users.json')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
     return (
         <div>
             <SectionTitle
-            title='Our Users'
+                title='Our Users'
             ></SectionTitle>
+
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+                {
+                    users.slice(0, 4).map(user => <UserCard
+                    key={user._id}
+                    user={user}
+                    ></UserCard>)
+                }
+            </div>
         </div>
     );
 };
