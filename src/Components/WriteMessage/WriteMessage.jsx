f0899719363a8e1b721a37d725f2d280b3f3f122
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-04-05 16:45:08 +0600

    added handleMessages function

--- src/Components/WriteMessage/WriteMessage.jsx
+++ src/Components/WriteMessage/WriteMessage.jsx
@@ -1,8 +1,30 @@
+import { useContext } from "react";
 import Button from "../Button/Button";
 import SectionTitle from "../SectionTitile/SectionTitle";
-
+import { MessageContext } from "../MessageProvider/MessageProvider";
+import Swal from 'sweetalert2'
 
 const WriteMessage = () => {
+
+    const { messages, setMessages } = useContext(MessageContext);
+
+    const handleMessages = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const message = e.target.message.value;
+
+        e.target.name.value = '';
+        e.target.message.value = '';
+
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your message has been saved",
+            showConfirmButton: false,
+            timer: 1500
+          });
+    }
+
     return (
         <div>
             <SectionTitle
@@ -10,19 +32,19 @@ const WriteMessage = () => {
             ></SectionTitle>
 
             <div>
-                
+
 
                 <div className="bg-base-200 w-1/2 my-5 mx-auto p-4">
-                    <form>
+                    <form onSubmit={handleMessages}>
                         <h2 className="text-blue-900 font-bold text-3xl ">Your Name: </h2>
                         <input type="text" placeholder="Your Name" name="name" className="border p-4 block my-4 w-full" required />
                         <br />
                         <h2 className="text-blue-900 font-bold text-3xl">Your Message: </h2>
                         <textarea name="message" id="message" placeholder="Your message" className="border p-4 block my-4 w-full" cols="30" rows="10"></textarea>
 
-                       <Button
-                       title='Submit'
-                       ></Button>
+                        <Button
+                            title='Submit'
+                        ></Button>
                     </form>
                 </div>
             </div>
